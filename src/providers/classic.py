e4881562e044db0660ee9acc4ff6acd173fc9708
Christos Alexiou <pcnoic@gmail.com>
2021-08-22 02:02:30 +0300

    fix send mail

--- src/providers/classic.py
+++ src/providers/classic.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
 import smtplib
 import os
 
@@ -9,6 +7,7 @@ import os
 class SMTPMail:
 
     try:
+        SMTP_USER = os.environ['SMTP_USER']
         SMTP_HOST = os.environ['SMTP_HOST']
         SMTP_PORT = os.environ['SMTP_PORT']
         SMTP_PWD = os.environ['SMTP_PASSWORD']
@@ -16,31 +15,36 @@ class SMTPMail:
         print ('Classic credentials are not set as environment variables.', e)
 
     def send(self, message):
+        sent_from = self.SMTP_USER
+        to = message.targets
+        subject = message.topic
+        body = message.message
 
-        # Create message object
+        email_text="""\
+            From: %s
+            To: %s
+            Subject: %s
 
-        msg = MIMEMultipart()
 
-        # Message parameters
+            %s
+        """%(sent_from,",".join(to),subject, body)
 
-        msg['From'] = message.sender
-        msg['To'] = message.targets
-        msg['Subject'] = message.topic
-
-        msg.attach(MIMEText(message.message, 'plain'))
-
-        srv = smtplib.SMTP(self.SMTP_HOST + ': ' + self.SMTP_PORT)
-        srv.starttls()
-        srv.login(msg['From'], self.SMTP_PWD)
-        srv.sendmail(msg['From'], msg['To'], msg.as_string())
-        srv.quit()
+        try:
+            smtp_server = smtplib.SMTP_SSL(self.SMTP_HOST, self.SMTP_PORT)
+            smtp_server.ehlo()
+            smtp_server.login(self.SMTP_USER, self.SMTP_PWD)
+            smtp_server.sendmail(sent_from, email_text)
+            smtp_server.close()
+            print("Mail was sent successfully.")
+        except Exception as e:
+            print("Something went wrong when sending mail: ", e)
 
 
 def push(message):
 
     # Extend when add more message types for the "classic" provider
 
-    switcher = {'email': SMTPMail}
+    switcher = {'email': SMTPMail()}
 
     sender = switcher.get(message.protocol, lambda : \
                           'This message protocol is not yet supported for this provider')
