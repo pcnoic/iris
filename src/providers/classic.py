7c1c05919c540187f53fd519969366e31ec99524
Christos Alexiou <pcnoic@gmail.com>
2021-08-23 12:49:02 +0300

    fix bug in sendmail

--- src/providers/classic.py
+++ src/providers/classic.py
@@ -33,11 +33,13 @@ class SMTPMail:
             smtp_server = smtplib.SMTP_SSL(self.SMTP_HOST, self.SMTP_PORT)
             smtp_server.ehlo()
             smtp_server.login(self.SMTP_USER, self.SMTP_PWD)
-            smtp_server.sendmail(sent_from, email_text)
+            smtp_server.sendmail(sent_from, to[0], email_text)
             smtp_server.close()
             print("Mail was sent successfully.")
+            return 0
         except Exception as e:
             print("Something went wrong when sending mail: ", e)
+            return 1
 
 
 def push(message):
@@ -48,4 +50,5 @@ def push(message):
 
     sender = switcher.get(message.protocol, lambda : \
                           'This message protocol is not yet supported for this provider')
-    sender.send(message)
+    result = sender.send(message)
+    return result
